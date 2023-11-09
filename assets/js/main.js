e92aee0eb1428dca646837ff7ca85625aac4551d
Danklif <josemanrenteria@gmail.com>
2023-11-09 16:16:35 -0500

    091123-01 Actualización proyectos y sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,6 +75,152 @@ function activePortfolio() {
 }
 linkPortfolio.forEach(link => link.addEventListener('click', activePortfolio))
 
+// Details
+const details = {
+    'portfolio-1': {
+        title: 'To-Do Machine',
+        category: 'Desarrollo web',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/work1.jpg',
+        projectUrl: 'https://draklif.github.io/TODO-Machine/'
+    },
+    'portfolio-2': {
+        title: 'Landing Page Asíncrona',
+        category: 'Desarrollo web',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/work2.jpg',
+        projectUrl: 'https://draklif.github.io/Async-Landing-Page/'
+    },
+    'portfolio-3': {
+        title: 'HooBank',
+        category: 'Diseño UI/UX',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/work3.jpg',
+        projectUrl: 'https://draklif.github.io/Modern-Bank/'
+    },
+    'portfolio-4': {
+        title: 'Clasrroom 2',
+        category: 'Diseño UI/UX',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/work4.jpg',
+        projectUrl: 'https://www.figma.com/file/XdQbqzUGJiIPEk5IZhSbXD/Classroom-2%3A-Recargado?type=design&node-id=1516%3A368&mode=design&t=gW4KP2OqbZ6dtgc8-1'
+    },
+    'portfolio-5': {
+        title: 'Easy Shop',
+        category: 'Estudios',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/work5.jpg',
+        projectUrl: 'https://draklif.github.io/EasyShop/'
+    },
+    'portfolio-6': {
+        title: 'Facturador VUE',
+        category: 'Estudios',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/work6.jpg',
+        projectUrl: 'https://draklif.github.io/TDW-TallerVue/'
+    },
+    'more-1': {
+        title: 'Identificador Gráfico',
+        category: 'NA',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: '',
+        projectUrl: ''
+    },
+    'more-2': {
+        title: 'Identificador Gráfico',
+        category: 'NA',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: '',
+        projectUrl: ''
+    },
+    'more-3': {
+        title: 'Identificador Gráfico',
+        category: 'NA',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/LogoBlack.png',
+        projectUrl: ''
+    },
+    'logo': {
+        title: 'Identificador Gráfico',
+        category: 'NA',
+        description: 'Descripción detallada del Proyecto Uno.',
+        imageUrl: 'assets/img/LogoBlack.png',
+        projectUrl: ''
+    },
+};
+
+// Dynamic portfolio
+const portfolioProjects = document.querySelectorAll('.portfolio__content');
+
+portfolioProjects.forEach(project => {
+    id = project.getAttribute('data-id')
+    project.children[1].children[0].textContent = details[id].category;
+    project.children[1].children[1].textContent = details[id].title;
+    project.children[0].href = details[id].projectUrl;
+    project.children[0].children[0].src = details[id].imageUrl;
+})
+
+// Sidebar
+const openDetailButtons = document.querySelectorAll('.button-link');
+const openMoreButtons = document.querySelectorAll('.more')
+const openLogoImage = document.getElementById('logo')
+const sidebar = document.getElementById('portfolio-sidebar');
+const closeSidebarButton = document.getElementById('close-sidebar');
+
+function openSidebar() {
+    sidebar.classList.add('portfolio-sidebar-active');
+}
+
+function closeSidebar() {
+    sidebar.classList.remove('portfolio-sidebar-active');
+}
+
+openDetailButtons.forEach(button => {
+    button.addEventListener('click', (event) => { 
+        event.preventDefault()
+        loadSidebar(details[event.target.parentElement.parentElement.getAttribute('data-id')])
+    })
+});
+
+openMoreButtons.forEach(button => {
+    button.addEventListener('click', (event) => { 
+        event.preventDefault()
+        loadSidebar(details[event.target.getAttribute('data-id')])
+    })
+});
+
+openLogoImage.addEventListener('click', (event) => { 
+    loadSidebar(details[event.target.getAttribute('data-id')])
+})
+
+function loadSidebar(project) {
+    // Actualiza la imagen
+    const imageElement = sidebar.querySelector('.portfolio-sidebar-image');
+    imageElement.src = project.imageUrl;
+    imageElement.alt = project.title;
+
+    // Actualiza el link
+    const linkElement = sidebar.querySelector('.portfolio-sidebar-link');
+    if (project.projectUrl) {
+        linkElement.href = project.projectUrl;
+        linkElement.textContent = 'Ir al proyecto'
+    } else {
+        linkElement.textContent = ''
+    }
+
+    // Actualiza el título
+    const titleElement = sidebar.querySelector('.portfolio-sidebar-title');
+    titleElement.textContent = project.title;
+
+    // Actualiza la descripción
+    const descriptionElement = sidebar.querySelector('.portfolio-sidebar-description');
+    descriptionElement.textContent = project.description;
+
+    openSidebar();
+}
+
+closeSidebarButton.addEventListener('click', closeSidebar);
+
 // GSAP
 gsap.from('.home__img', { opacity: 0, duration: 2, delay: .5, x: 60 })
 gsap.from('.home__data', { opacity: 0, duration: 2, delay: .8, y: 25 })
